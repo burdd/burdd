637fe35982c9a30d1e2473882bc6791fc152502f
Otutochi Nwadinkpa <o.caleb.nwa@gmail.com>
2025-11-25 03:24:24 -0600

    added useNavigate import

--- client/src/pages/projects/pages/MembersPage.jsx
+++ client/src/pages/projects/pages/MembersPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import EmptyState from '@/common/EmptyState/EmptyState';
 import Select from '@/common/Select/Select';
 import { getProjectById, searchUsers, addMemberToProject, removeMemberFromProject, changeMemberRole } from '@/api';
